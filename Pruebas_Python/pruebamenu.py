96ac37cf1b3160c897eb44dd1ebd690a2da6aea6
Diego García <168243435+dg203302@users.noreply.github.com>
2026-04-16 21:14:03 -0300

    Update pruebamenu.py with new API URL and request parameters for improved data retrieval

--- Pruebas_Python/pruebamenu.py
+++ Pruebas_Python/pruebamenu.py
@@ -1,12 +1,13 @@
 import requests
+import time
+API_URL = "https://proxyrt-production.up.railway.app/arrivals"
 
-#API_URL = "https://proxy-rt-api-production.up.railway.app/arrivals"
-API_URL = "http://0.0.0.0:8000/arrivals"
+#API_URL = "http://0.0.0.0:8000/arrivals"
+#API_URL = "https://proxy-rt.onrender.com/arrivals"
 datos_consulta = {
-    "url": "https://moovitapp.com/tripplan/san_juan-6137/lines/10/40197828/5701125/es-419?customerId=NPIdiV-P9Gcj-pA7yOXVPg",
-    "id_p": "stop-45379671-0"
+    "url": "https://moovitapp.com/tripplan/san_juan-6137/lines/130/73818526/6607768/es-419?customerId=NPIdiV-P9Gcj-pA7yOXVPg",
+    "id_p": "stop-46329029-0"
 }
-
 print(f"Consultando a la Red Tulum para la línea {datos_consulta['url']}...")
 
 try:
@@ -20,6 +21,7 @@ try:
     print("\n¡Respuesta exitosa!")
     print(f"Mensaje: {datos_recibidos.get('horario_estimado', 'Sin mensaje')}")
     print("Arribos:", datos_recibidos.get('arrivals', []))
+    print(f"Tiempo de respuesta: {respuesta.elapsed.total_seconds():.2f} segundos")
 
 except requests.exceptions.RequestException as e:
     print(f"\n❌ Hubo un error de conexión: {e}")
