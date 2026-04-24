6bd2bbcd2315fda945b7400afb67755556c4923e
Diego García <168243435+dg203302@users.noreply.github.com>
2026-04-24 17:27:19 -0300

    Refactor code structure for improved readability and maintainability

--- paradas_tulum/obtenedor_paradas_tulum.py
+++ paradas_tulum/obtenedor_paradas_tulum.py
@@ -4,10 +4,33 @@ from pathlib import Path
 from playwright.sync_api import sync_playwright
 latitud = -31.5375
 longitud = -68.5364
-timeout_default=5000
+timeout_default=2000
 ACTION_TIMEOUT_MS = 15000
 numeros_de_lineas = [
-    "130"
+    "2", "3", "4", "20", "30", "40",
+    "100", "101", "102", "103", "104",
+    "120", "122", "123", "124", "125", "126", "127",
+    "140", "141", "142",
+    "160", "161", "162",
+    "200", "201", "202", "203", "204", "205", "206", "207", "208", "209", "210", "211", "212","213", "214",
+    "240", "241", "242", "243", "244", "245", "246",
+    "260", "261", "262", "263", "264", "265", "266",
+    "300", "301", "302", "303", "304", "305",
+    "320", "321", "322", "323",
+    "340", "341", "342", "343", "344", "345", "346",
+    "360", "361", "362", "363", "364",
+    "400", "401", "402", "403", "404", "405", "406", "407", "408",
+    "420", "421", "422", "423",
+    "440", "441", "442", "443", "444",
+    "460", "461", "462",
+    "500", "501", "502", "503", "504", "505",
+    "560",
+    "600", "601", "602",
+    "700", "701", "702",
+    "800", "801", "802", "808",
+    "850",
+    "A", "B", "C", "D", "E",
+    "TEO1", "TEO2", "TEO3", "TNS",
 ]
 Paradas_por_linea = {}
 def _guardar_json(salida, data):
@@ -43,7 +66,7 @@ def main():
             for linea in numeros_de_lineas:
                 try:
                     pag.goto('https://moovitapp.com/tripplan/san_juan-6137/lines/es-419?customerId=NPIdiV-P9Gcj-pA7yOXVPg')
-                    pag.wait_for_timeout(timeout_default)
+                    pag.wait_for_timeout(timeout_default+1000)
                     buscador = pag.get_by_placeholder('Buscar una línea')
                     buscador.fill(linea)
                     buscador.press('Enter')
